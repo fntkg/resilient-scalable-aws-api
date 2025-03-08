f756dc1d7f8e425781a1abf2f1ecf84e0359609c
German <garces.g1999@gmail.com>
2025-03-08 08:25:17 +0100

    fix: fix eks definition

--- resilient_scalable_aws_api/resilient_scalable_aws_api_stack.py
+++ resilient_scalable_aws_api/resilient_scalable_aws_api_stack.py
@@ -3,6 +3,7 @@ from aws_cdk import (
     aws_ec2 as ec2,
     aws_eks as eks,
 )
+from aws_cdk.lambda_layer_kubectl_v32 import KubectlV32Layer
 from constructs import Construct
 import yaml
 import os
@@ -23,24 +24,35 @@ class ResilientScalableAwsApiStack(Stack):
 
         # Create the EKS cluster within the VPC
         cluster = eks.Cluster(
-            # TODO : Revisar que este usando las instancias gratis
-            # TODO : Configurar autoscaling
             self, "EksCluster",
-            version=eks.KubernetesVersion.V1_21,
+            version=eks.KubernetesVersion.V1_32,
+            kubectl_layer=KubectlV32Layer(self, "kubectl"),
             vpc=vpc,
-            default_capacity=2,  # Adjust the default capacity if needed
-            default_capacity_instance="t2.large"
+            default_capacity = 0,
+        )
+
+        # Create a Managed Node Group in multiple AZs
+        cluster.add_auto_scaling_group_capacity(
+            "EksNodeGroup",
+            instance_type=ec2.InstanceType("t3.micro"),
+            min_capacity=3,
+            desired_capacity=3,
+            max_capacity=6,
+            vpc_subnets={"subnet_type": ec2.SubnetType.PUBLIC}  # Multiple AZs
         )
 
         # Load external Kubernetes manifests
-        deployment_manifest = self.load_yaml("manifests/deployment.yaml")
-        service_manifest = self.load_yaml("manifests/service.yaml")
-        hpa_manifest = self.load_yaml("manifests/hpa.yaml")
+        deployment_manifest = self.load_yaml("k8s_manifests/deployment.yml")
+        service_manifest = self.load_yaml("k8s_manifests/service.yml")
+        hpa_manifest = self.load_yaml("k8s_manifests/hpa.yml")
 
         # Apply the Kubernetes manifests to the cluster
         cluster.add_manifest("ApiDeployment", deployment_manifest)
         cluster.add_manifest("ApiService", service_manifest)
+        # TODO : Asegúrate de tener instalado el metrics-server en tu clúster, ya que el HPA lo requiere para obtener las métricas de CPU.
         cluster.add_manifest("ApiHpa", hpa_manifest)
+
+        # TODO : Establecer alerting
 
     @staticmethod
     def load_yaml(file_path: str):
